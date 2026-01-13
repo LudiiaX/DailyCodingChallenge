e05e3e2609c86520c4ac43709016ee4c1234829f
LudiiaX <mathis50.gilles50@gmail.com>
2026-01-13 16:30:36 +0100

    j 13

---
+++ freeCodeCamp/01-2026/13-01-2026.py
@@ -0,0 +1,8 @@
+def odd_or_even(n):
+    return "Odd" if n % 2 else "Even"
+
+print(odd_or_even(1))
+print(odd_or_even(2))
+print(odd_or_even(13))
+print(odd_or_even(196))
+print(odd_or_even(123456789))
